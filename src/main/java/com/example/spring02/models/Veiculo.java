57f9307f6d31bb18bcad930016d352fc1a67c132
marinfarias <marina.farias@mercadolivre.com>
2022-07-04 17:26:08 -0300

    first commit

---
+++ src/main/java/com/example/spring02/models/Veiculo.java
@@ -0,0 +1,19 @@
+package com.example.spring02.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Veiculo {
+
+    private String placa;
+    private String modelo;
+    private double preco;
+
+
+}
---
+++ src/main/java/com/example/spring02/repository/VeiculoRepo.java
@@ -0,0 +1,69 @@
+package com.example.spring02.repository;
+
+import com.example.spring02.models.Veiculo;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class VeiculoRepo {
+
+    private final String linkFile = "src/main/resources/dados.json";
+
+    public Veiculo getVeiculo(String placa) {
+
+        List<Veiculo> lista = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            lista = Arrays.asList
+                    (mapper.readValue(new File(linkFile), Veiculo[].class));
+            for (Veiculo v: lista){
+                if(v.getPlaca().equals(placa)){
+                    return v;
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Erro");
+        }
+        return null;
+    }
+
+
+
+    public List<Veiculo> getAllVeiculo() {
+        List<Veiculo> lista = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            lista = Arrays.asList
+                    (mapper.readValue(new File(linkFile), Veiculo[].class));
+        } catch (Exception ex) {
+            System.out.println("Erro");
+        }
+
+        return lista;
+    }
+
+    public void saveVeiculo(Veiculo novoVeiculo){
+        List<Veiculo> listaAtual = null;
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+        try{
+            listaAtual = Arrays.asList
+                    (mapper.readValue(new File(linkFile), Veiculo[].class));
+            List<Veiculo> listaCopia = new ArrayList<>(listaAtual);
+            listaCopia.add(novoVeiculo);
+            writer.writeValue(new File(linkFile), listaCopia);
+
+        } catch (Exception ex) {
+            System.out.println("Erro");
+
+        }
+
+    }
+}
